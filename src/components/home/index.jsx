60adb7b397861d1991ce1937c6574a73875bc79e
Murodil <murodil01@gmail.com>
2025-07-27 20:39:13 +0500

    side padding

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -21,11 +21,7 @@ const AppLayout = () => {
             onToggleDesktop={() => setCollapsed(!collapsed)}
             onToggleMobile={() => setIsMobileOpen(true)}
           />
-<<<<<<< HEAD
           <main className="flex-1 overflow-y-auto bg-gray-100 p-4 pr-4">
-=======
-          <main className="flex-1 overflow-y-auto bg-[#F2F2F2] p-4 pr-8">
->>>>>>> 916c5b010183979b3a0a63cbf2f23551b88e2511
             <Outlet />
           </main>
         </div>
