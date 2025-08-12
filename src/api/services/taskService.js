134faf1f89b005d8c187059c84419d201fd897df
Jasur Abdisoatov <jasurabdisoatov03@gmail.com>
2025-08-12 18:46:49 +0500

    inner_circle design changed

--- src/api/services/taskService.js
+++ src/api/services/taskService.js
@@ -6,10 +6,4 @@ export const getTaskById = (id) => api.get(endpoints.tasks.getById(id));
 export const createTask = (data) => api.post(endpoints.tasks.create, data);
 export const updateTask = (id, data) => api.put(endpoints.tasks.update(id), data);
 export const deleteTask = (id) => api.delete(endpoints.tasks.delete(id));
-
-
 export const getProjectTaskById = (id) => api.get(endpoints.projects.getByIdTasks(id));
-
-
-
-
