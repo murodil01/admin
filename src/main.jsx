b8294730ccb127cde6e346674323ebc0068ba763
Murodil <murodil01@gmail.com>
2025-08-13 13:18:30 +0500

    login

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import "@ant-design/v5-patch-for-react-19";
 import { Toaster } from "react-hot-toast";
+import "aos/dist/aos.css";   // AOS CSS import qilinadi
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
