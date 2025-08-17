7e6e63190295f3013923a03da2e324eb8cc409f1
Admiral2000 <husniddinzoirjonov@gmail.com>
2025-08-17 17:12:57 +0500

    corrected all page in Project

--- src/api/base.js
+++ src/api/base.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true, // Enable credentials for all requests
-    // headers: {
-    //     "Content-Type": "application/json",
-    // },
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 // Request interceptor
