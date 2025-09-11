9cb02e43a7b055fedb73f639a718912e005aeeda
Admiral2000 <husniddinzoirjonov@gmail.com>
2025-09-11 18:49:36 +0500

    changed status bg

--- src/components/lead-parts/group-section/status-board/index.jsx
+++ src/components/lead-parts/group-section/status-board/index.jsx
@@ -262,7 +262,7 @@ const StatusDropdown = ({
 
       {/* Enhanced Add Status Modal */}
       {showAddModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[60]">
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-[60]">
           <div
             className="bg-white rounded-lg p-6 w-96 max-w-[90vw] shadow-xl"
             ref={modalRef}
